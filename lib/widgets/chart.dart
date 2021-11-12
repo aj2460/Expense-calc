a32203321c641616a80b554048157c88a2b76627
Anand <ajkadakkal@gmail.com>
2021-11-12 22:58:23 +0530

    create char widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,7 +1,28 @@
 import 'package:flutter/material.dart';
+import "package:intl/intl.dart";
+
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
-  const Chart({Key? key}) : super(key: key);
+  final List<Transaction> recentTransactions;
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(7, (index) {
+      final weekDay = DateTime.now().subtract(Duration(days: index));
+      var totalSum = 0.0;
+      for (int i = 0; i < recentTransactions.length; i++) {
+        var day = recentTransactions[i];
+        if (day.date.day == weekDay.day &&
+            day.date.month == weekDay.month &&
+            day.date.year == weekDay.year) {
+          totalSum += recentTransactions[i].amount;
+        }
+      }
+
+      return {"day": DateFormat.E(weekDay), "amount": totalSum};
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
