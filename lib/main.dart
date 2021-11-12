d8b6faed6e95442adc89cfcf400d3eee2a4a1b0b
Anand <ajkadakkal@gmail.com>
2021-11-12 14:27:18 +0530

    Add transaction class and display it in a card

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './transaction.dart';
 
 void main() => runApp(MyApp());
 
@@ -13,25 +14,42 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
+  final List<Transaction> transactions = [
+    Transaction(
+        id: "t1", title: "New Shoes", amount: 69.99, date: DateTime.now()),
+    Transaction(
+        id: "t2",
+        title: "Weekly Groceries",
+        amount: 19.99,
+        date: DateTime.now()),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("My App"),
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Container(
-              width: double.infinity,
-              child: Card(
-                color: Color.fromRGBO(128, 128, 128, .5),
-                child: Text("CHART"),
-              ),
+      appBar: AppBar(
+        title: Text("My App"),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            child: Card(
+              color: Color.fromRGBO(128, 128, 128, .5),
+              child: Text("CHART"),
             ),
-            Text("Transaction")
-          ],
-        ));
+          ),
+          Column(
+            children: transactions.map((tx) {
+              return Card(
+                child: Text(tx.title),
+              );
+            }).toList(),
+          )
+        ],
+      ),
+    );
   }
 }
