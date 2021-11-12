b8f2708242e0e301d96748c13bc16bf385a23a88
Anand <ajkadakkal@gmail.com>
2021-11-12 18:05:42 +0530

    Remove scroll added to transaction  list  keep overall scroll

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -8,48 +8,42 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 300,
-      child: SingleChildScrollView(
-        child: Column(
-          children: _userTransactions.map((tx) {
-            return Card(
-              child: Row(
-                children: [
-                  Container(
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    padding: EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.purple),
-                    ),
-                    child: Text(
-                      tx.amount.toString(),
-                      style: TextStyle(
-                          fontSize: 24,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.purple),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        tx.title,
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.bold),
-                      ),
-                      Text(
-                        DateFormat.yMMMd().format(tx.date),
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
+    return Column(
+      children: _userTransactions.map((tx) {
+        return Card(
+          child: Row(
+            children: [
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                padding: EdgeInsets.all(4),
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.purple),
+                ),
+                child: Text(
+                  tx.amount.toString(),
+                  style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.purple),
+                ),
               ),
-            );
-          }).toList(),
-        ),
-      ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    tx.title,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    DateFormat.yMMMd().format(tx.date),
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                ],
+              )
+            ],
+          ),
+        );
+      }).toList(),
     );
   }
 }
